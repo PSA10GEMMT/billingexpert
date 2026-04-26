f520231b09592a5c6b5a1d512609ed60cd5ad66f
Jace Sullivan <JACE.R.SULLIVAN@GMAIL.COM>
2026-04-25 18:29:18 -0600

    clean up helper scripts

--- fix_hint.py
+++
@@ -1,8 +0,0 @@
-with open('timeline.html', 'r') as f:
-    html = f.read()
-
-html = html.replace('<p class="click-hint"><i class="fas fa-hand-pointer"></i> Click any detail to expand</p>', '')
-
-with open('timeline.html', 'w') as f:
-    f.write(html)
-print('Click hint removed!')
